39f6227c1eaabb95a7c40b6ebf9731de68cc0ac1
mediator007@mail <mediator007@mail.ru>
2023-03-05 21:07:17 +0300

    for bot

--- prediction.py
+++ prediction.py
@@ -5,8 +5,3 @@ def prediction(question):
     pipeline = joblib.load('pipline.pkl')
     answer = pipeline.predict([question])[0]
     return answer
-
-if __name__=='__main__':
-    print('Answer --------- :', prediction('hi'))
-    print('Answer --------- :', prediction('зачем а главное'))
-    print('Answer --------- :', prediction('как заказать услугу и заключить договор'))
